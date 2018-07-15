11b94ec748eb7b4cee5edff3778d534f59ec44aa
miki1989 <mikolajheller@gmail.com>
2018-07-15 17:31:24 +0200

    Corrected version. Added for loops to check equality.

--- src/ArrayComparator.java
+++ src/ArrayComparator.java
@@ -1,12 +1,21 @@
 public class ArrayComparator {
 
     public boolean compare(int [][] array1, int [][] array2){
-        if(array1.equals(array2)){
-            return true;
-        }
-        else{
+        if(array1.length != array2.length) {
             return false;
         }
+        for(int i = 0; i < array1.length; i++){
+            if (array1[i].length != array2[i].length){
+                return false;
+            }
+            for(int j = 0; j < array1[i].length; j++){
+                if(array1[i][j] != array2[i][j]){
+                    return false;
+                }
+            }
+
+        }
+        return true;
     }
 
 
--- src/ArrayTest.java
+++ src/ArrayTest.java
@@ -1,8 +1,8 @@
 public class ArrayTest {
     public static void main(String[] args) {
 
-            int [][] tab1 = new int[2][2];
-            int [][] tab2 = new int[2][2];
+            int [][] tab1 = {{1,2,3,4},{5,6,7,8},{9,10,11,12}};
+            int [][] tab2 = {{1,2,3,4},{5,6,7,9},{9,10,11,12}};
             ArrayComparator comparator = new ArrayComparator();
         System.out.println(comparator.compare(tab1,tab2));
 
