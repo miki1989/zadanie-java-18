7aba0347f8d88e9bbf1b63eac66c1bc30fac7bfe
miki1989 <mikolajheller@gmail.com>
2018-07-15 17:33:03 +0200

    Corrected version. Added for loops to check equality. Test

--- src/ArrayTest.java
+++ src/ArrayTest.java
@@ -1,10 +1,15 @@
 public class ArrayTest {
     public static void main(String[] args) {
 
-            int [][] tab1 = {{1,2,3,4},{5,6,7,8},{9,10,11,12}};
-            int [][] tab2 = {{1,2,3,4},{5,6,7,9},{9,10,11,12}};
-            ArrayComparator comparator = new ArrayComparator();
+        int [][] tab1 = {{1,2,3,4},{5,6,7,8},{9,10,11,12}};
+        int [][] tab2 = {{1,2,3,4},{5,6,7,9},{9,10,11,12}};
+        ArrayComparator comparator = new ArrayComparator();
         System.out.println(comparator.compare(tab1,tab2));
+
+        int [][] tab3 = {{1,2,3,4},{5,6,7,8},{9,10,11,12}};
+        int [][] tab4 = {{1,2,3,4},{5,6,7,8},{9,10,11,12}};
+        ArrayComparator comparator1 = new ArrayComparator();
+        System.out.println(comparator.compare(tab3,tab4));
 
     }
 }
